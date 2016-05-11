eaad0a1bf5972f8ffde48161659eb577c0aaf14c
Sudan Vellakovil Kanaka Vel <sudan.kanakavel@maplebots.com>
2016-05-11 15:00:32 +0530

    Add files via upload

--- app/index.js
+++ app/index.js
@@ -1,13 +1,15 @@
 var  React = require('react')
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+//1.1.App Title component starts here
+
+var BigOne= React.createClass({
 
 render: function ()  {
 
 return (
 
-<div> Dail attendence  app component 1!! </div>
+<div> App title component !! </div>
 
 	)
 
@@ -17,11 +19,60 @@ return (
 
 });
 
+//1.1.App Title components ends here
+
+//1.2.App search component starts here
+
+var BigTwo= React.createClass({
+
+render: function ()  {
+
+return (
+
+<div> App seasrch component !! </div>
+
+	)
+
+
+}
+
+
+
+});
+
+//1.2.App search component ends here
+
+//  1.header component starts here
+
+var BigAll= React.createClass({
+
+render: function ()  {
+
+return (
+
+<div> <BigOne /> <BigTwo /> </div>
+
+	)
+
+
+}
+
+
+
+});
+
+// 1.header component ends here
+
+
+
 
 ReactDOM.render(
 
-<HelloWorld  />,
+<BigAll  />,
 
 document.getElementById('app')  
 
-);
+); 
+
+
+
